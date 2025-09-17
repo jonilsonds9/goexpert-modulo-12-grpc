9680677a1f7a19854fc20c23a6f275122945f86e
Jonilson Sousa <jonilsonds9@gmail.com>
2025-09-17 20:38:50 -0300

    Listando categories

--- internal/services/category.go
+++ internal/services/category.go
@@ -32,3 +32,22 @@ func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCateg
 
 	return categoryResponse, nil
 }
+
+func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb.CategoryList, error) {
+	categories, err := c.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var categoriesResponse []*pb.Category
+
+	for _, category := range categories {
+		categoriesResponse = append(categoriesResponse, &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		})
+	}
+
+	return &pb.CategoryList{Categories: categoriesResponse}, nil
+}
